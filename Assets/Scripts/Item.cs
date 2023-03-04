6bcc99f3123b3b2f88d6a81e129a9a96ae5b1f1a
Stephen Pearce <hello@stevepdp.dev>
2023-03-04 13:06:36 +0000

    feat(item): display scriptable object items

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,25 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum ItemType
+{
+    Heart,
+    Mana,
+    Bow,
+    Arrows,
+    Sword,
+    Shield,
+    Armour,
+    Tunic
+}
 
 [CreateAssetMenu(fileName = "NewItem", menuName = "Shop/Add Item", order = 0)]
 public class Item : ScriptableObject
 {
-    [SerializeField] ItemType itemType;
-    [SerializeField] new string name;
-    [SerializeField] int cost;
-    [SerializeField] Color color; // tmp representation
-    [SerializeField] Sprite icon; // ...later
-    
-    enum ItemType
-    {
-        Heart,
-        Mana,
-        Bow,
-        Arrows,
-        Sword,
-        Shield,
-        Armour,
-        Tunic
-    }
+    public ItemType itemType;
+    public new string name;
+    public int cost;
+    public Color color; // tmp representation
+    public Image image; // ...later
+    public bool backpackStatus;
 }
---
+++ Assets/Scripts/ItemCard.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCard : MonoBehaviour
+{
+    [SerializeField] TMP_Text displayNameText;
+    [SerializeField] Image displayImage;
+    [SerializeField] TMP_Text displayCostText;
+    [SerializeField] TMP_Text displayBackpackStatusText;
+    [SerializeField] Item itemScriptableObject;
+
+    new string name;
+    Image image;
+    Color colourFallback;
+    int cost;
+    ItemType itemType;
+    bool inBackpack;
+
+    void Awake()
+    {
+        displayImage = displayImage.GetComponent<Image>();
+        SetCard();
+    }
+        
+    void Start() => ShowCard();
+
+    void SetCard()
+    {
+        name = itemScriptableObject?.name;
+        image = itemScriptableObject?.image;
+        colourFallback = (Color) itemScriptableObject?.color;
+        cost = (int) itemScriptableObject?.cost | 0;
+        itemType = (ItemType) itemScriptableObject?.itemType;
+        inBackpack = (bool) itemScriptableObject?.backpackStatus;
+    }
+
+    void ShowCard()
+    {
+        if (displayNameText != null)
+            displayNameText.text = name;
+
+        if (!displayImage.sprite)
+        {
+            // use colour fallback and set alpha to max
+            displayImage.color = (Color) itemScriptableObject.color;
+            var tmpColour = displayImage.color;
+            tmpColour.a = 1f;
+            displayImage.color = tmpColour;
+        }
+
+        if (displayCostText != null)
+            displayCostText.text = cost.ToString();
+
+        if (displayBackpackStatusText != null)
+            displayBackpackStatusText.text = inBackpack ? "B" : "";
+    }   
+}
